f84efd9d335eef59299fa40910ad9dc82b20e946
DHtestteam <63408810+DHtestteam@users.noreply.github.com>
2020-04-22 15:06:30 -0400

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,25 @@
+// An example configuration file.
+exports.config = {
+   directConnect: true,
+
+   // Capabilities to be passed to the webdriver instance.
+   capabilities: {
+      'browserName': 'chrome'
+   },
+
+   // Framework to use. Jasmine is recommended.
+   framework: 'jasmine',
+
+   // Spec patterns are relative to the current working directory when
+   // protractor is called.
+   specs: ['TestSpecification.js'],
+   //framework: "jasmine2", //must set it if you use JUnitXmlReporter
+   onPrepare: function(){ //configure junit xml report
+      var jasmineReporters = require('jasmine-reporters');
+      jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+         consolidateAll: true,
+         filePrefix: 'guitest-xmloutput',
+         savePath: 'reports'
+      }));
+   },
+};
